4b1995b7e87521eb48bbffa75922a6d3dd284853
Cycoder <mnerren@gmail.com>
2022-05-10 18:21:00 -0700

    new kata

---
+++ 7kyu/closestMultiple10.js
@@ -0,0 +1,18 @@
+// 7 kyu Return the closest number multiple of 10
+//
+// Given a number return the closest number to it that is divisible by 10.
+//
+// Example input:
+//
+// 22
+// 25
+// 37
+//
+// Expected output:
+//
+// 20
+// 30
+// 40
+//
+// Answer:
+const closestMultiple10 = num => Math.round(num / 10) * 10;
