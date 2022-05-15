418acc85763c0fbcd9053915da616064e6da7ea0
Cycoder <mnerren@gmail.com>
2022-05-15 14:57:01 -0700

    new kata

---
+++ 8kyu/sentenceSmash.js
@@ -0,0 +1,10 @@
+// 8 kyu Sentence Smash
+//
+// Sentence Smash
+// 
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+// You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. 
+// Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//
+// Answer:
+const smash = words => words.join(' ').trim()
