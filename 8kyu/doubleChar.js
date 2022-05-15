834af9d8b1a5acc061559598d4ed663b70bc39e2
Cycoder <mnerren@gmail.com>
2022-05-15 15:08:21 -0700

    new kata

---
+++ 8kyu/doubleChar.js
@@ -0,0 +1,13 @@
+// 8 kyu Double Char
+//
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// Examples (Input -> Output):
+// 
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// 
+// Good Luck!
+//
+// Answer:
+const doubleChar = str => Array.from(str, (e)=>e+e).join('');
