cfce70ea094831098fc0b801151e5437166c8557
Cycoder <mnerren@gmail.com>
2022-05-15 14:54:01 -0700

    upd

--- 8kyu/isItAPalindrome.js
+++ 8kyu/isItAPalindrome.js
@@ -3,3 +3,6 @@
 // Write a function that checks if a given string (case insensitive) is a palindrome.
 // Answer:
 const isPalindrome = x => x.toLowerCase().split('').reverse().join('') === x.toLowerCase();
+
+// shorter
+const isPalindrome = x => [...x].reverse().join('').toLowerCase() === x.toLowerCase();
