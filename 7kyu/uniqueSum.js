d819d82a350a4782dd8a648ef4c3285b6677c118
Cycoder <mnerren@gmail.com>
2022-05-16 19:07:25 -0700

    new kata

---
+++ 7kyu/uniqueSum.js
@@ -0,0 +1,17 @@
+// 7 kyu Unique Sum
+//
+// Given a list of integers values, your job is to return the sum of the values; 
+// however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
+// 
+// For example:
+// 
+// [ 1, 2, 3] ==> 6
+// 
+// [ 1, 3, 8, 1, 8] ==> 12
+// 
+// [ -1, -1, 5, 2, -7] ==> -1
+// 
+// [] ==> null
+//
+// Answer:
+const uniqueSum = lst => Array.from(new Set(lst)).reduce((a,e)=>a+=e,null);
