21088b9a2e98dc58bfdf62719be342565ed85aae
Cycoder <mnerren@gmail.com>
2022-05-17 12:51:07 -0700

    new kata

---
+++ 6kyu/nuclearMissileManager.js
@@ -0,0 +1,13 @@
+// 6 kyu Nuclear Missile Manager
+//
+// Our nuclear missile manager system is coded in Node.js. 
+// We are currently testing the system and it seems that the launchAll function does not work as expected. 
+// It should launch 5 missiles each 1 second apart. 
+// The current code tries to launch the missile #5 five times...
+// 
+// Can you fix this for us? You know, it's pretty critical code...
+// 
+// Note: There are 5 missiles labeled i which is a number in {0, 1, 2, 3, 4}. The missile i should be launched after i seconds.
+
+// Answer:
+const launchAll = (lm, i = -1) => {for(let k = 0; k < 5; k++) setTimeout(() => lm(++i), k * 1000)};
