9411dd1799db59b23c43acdb1150f21adfa2c98e
Cycoder <mnerren@gmail.com>
2022-05-18 19:55:41 -0700

    upd

--- 3kyu/_template copy.js
+++ 3kyu/_template copy.js
@@ -6,6 +6,8 @@ function solution(input) {
     return input;
 }
 
+// Do this via old timey long multiplication
+
 
 function cleanInput(val) {
 
@@ -21,11 +23,18 @@ function cleanInput(val) {
 
     // both leading and trailing 0
     "00014000".replace(/^0+|\.0+$/g, ''); 
-    val = val.replace(/^0+|\.0+$/g, '');
+    val = val.replace(/^0+|\.*0+$/g, '');
 
     // strip off trailing decimal (.)
     "00014000.".replace(/\.+$/, '');    // finally this
     val = val.replace(/\.+$/, '');
+
+    // Make .01 into 0.01  MIGHT NOT BE NEEDED, TEST
+    val = val.replace(/^-[\.]+/, '-0.');
+    val = val.replace(/^\.+/, '0.');
+
+
+    val = val==='' || val==='-' ? 0 : val;
 
     console.log(val);
     return val;
