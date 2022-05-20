da30677a83c5acfbd63761c26c90dedbf67cd045
Cycoder <mnerren@gmail.com>
2022-05-20 12:15:03 -0700

    upd

--- 3kyu/_template copy.js
+++ 3kyu/_template copy.js
@@ -10,6 +10,7 @@ function solution(input) {
 
 
 function cleanInput(val) {
+    if(val.indexOf('.') != val.lastIndexOf('.')) return 0;
 
     // negative with leading 0 (will turn '-000' into '-', check for this)
     "-00014".replace(/^-[0]+/, '-'); // do this first
@@ -18,21 +19,24 @@ function cleanInput(val) {
     // positive with leading 0 (will turn '000' into '', check for this)
     // "00014".replace(/^0+/, '');
 
-    // strip off trailing 0
-    // "00014000".replace(/0+$/, '');
-
     // both leading and trailing 0
     "00014000".replace(/^0+|\.0+$/g, ''); 
-    val = val.replace(/^0+|\.*0+$/g, '');
+    // val =  val.replace(/^0+|\.*0+$/g, '');  // this has * which removes trailing zeros
+    
+    // leading 0
+    val =  val.replace(/^0+/g, '');
+
+    // trailing 0
+    val =  val.replace(/(\.[0-9]*[1-9])0+$|\.0*$/,'$1');
+
 
     // strip off trailing decimal (.)
     "00014000.".replace(/\.+$/, '');    // finally this
-    val = val.replace(/\.+$/, '');
+    val =   val.replace(/\.+$/, '');
 
     // Make .01 into 0.01  MIGHT NOT BE NEEDED, TEST
     val = val.replace(/^-[\.]+/, '-0.');
     val = val.replace(/^\.+/, '0.');
-
 
     val = val==='' || val==='-' ? 0 : val;
 
