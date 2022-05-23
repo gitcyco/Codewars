08254f6775342d76ec33aa107d160b736813cf85
Cycoder <mnerren@gmail.com>
2022-05-22 18:55:48 -0700

    new kata

---
+++ 6kyu/stringArrayDuplicates.js
@@ -0,0 +1,16 @@
+// 6 kyu String array duplicates
+//
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+// 
+// For example:
+// 
+//     dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// 
+//     dup(["kelless","keenness"]) = ["keles","kenes"].
+// 
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+// 
+// Good luck!
+//
+// Answer:
+const dup = s => s.map(e=> [...e].reduce((a,e) => (a[a.length-1] !== e ? a.push(e) : a, a) ,[]).join(''));
