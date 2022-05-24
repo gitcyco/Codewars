4e8068086393501a9045bc2eab091e7ac6331f12
Cycoder <mnerren@gmail.com>
2022-05-24 03:17:02 -0700

    new kata

---
+++ 6kyu/uniqueInOrder.js
@@ -0,0 +1,20 @@
+// 6 kyu Unique In Order
+//
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
+// without any elements with the same value next to each other and preserving the original order of elements.
+// 
+// For example:
+// 
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+//
+// Answer:
+const uniqueInOrder=function(it, current, result=[]){
+    for(let i = 0; i < it.length; i++) {
+      if(it[i] == current) continue;
+      result.push(it[i]);
+      current = it[i]
+    }
+    return result;
+}
