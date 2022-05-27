daf954f912192d65c1e3c01c648cc28c3856b76d
Cycoder <mnerren@gmail.com>
2022-05-27 12:51:31 -0700

    new kata

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,6 @@
+// 8 kyu MakeUpperCase
+//
+// Write a function which converts the input string to uppercase.
+//
+// Answer:
+const makeUpperCase = s => [...s].map(e=>e.charCodeAt(0)).map(e=> e>96&&e<123 ? String.fromCharCode(e-32) : String.fromCharCode(e)).join('');
