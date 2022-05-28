4712f72d1e71bed6f29e9bbd5ec08f534d564b0d
Cycoder <mnerren@gmail.com>
2022-05-27 17:09:05 -0700

    new kata

---
+++ 5kyu/beeramid.js
@@ -0,0 +1,32 @@
+// 5 kyu Beeramid
+//
+// Let's pretend your company just hired your friend from college and paid you a referral bonus. 
+// Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral 
+// bonus to buy, and build, the largest three-dimensional beer can pyramid you can. 
+// And then probably drink those beers, because let's pretend it's Friday too.
+// 
+// A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+// 
+// Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+// 
+//     your referral bonus, and
+// 
+//     the price of a beer can
+// 
+// For example:
+// 
+// beeramid(1500, 2); // should === 12
+// beeramid(5000, 3); // should === 16
+//
+// Answer:
+//
+const beeramid = function(bonus, price) {
+    let beers = Math.floor(bonus/price);
+    let level = 0;
+    if(beers < 1) return 0;
+    while (beers > 0) {
+      level++
+      beers = beers - (level * level);
+    }
+    return beers == 0 ? level : level - 1; 
+}
