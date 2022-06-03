e00ab7c7ce3424aea2382c5f103688cf81ad7f1f
Cycoder <mnerren@gmail.com>
2022-06-02 17:48:04 -0700

    new kata

---
+++ 7kyu/removeDuplicateWords.js
@@ -0,0 +1,16 @@
+// 7 kyu Remove duplicate words
+//
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+// 
+// Example:
+// 
+// Input:
+// 
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+// 
+// Output:
+// 
+// 'alpha beta gamma delta'
+//
+// Answer:
+const removeDuplicateWords = s => Array.from(new Set(s.split(' '))).join(' ');
