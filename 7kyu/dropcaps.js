2bb30b801dee8437beb949f39a617074e159ab2c
Cycoder <mnerren@gmail.com>
2022-06-07 18:36:03 -0700

    new kata

---
+++ 7kyu/dropcaps.js
@@ -0,0 +1,20 @@
+// 7 kyu Dropcaps
+//
+// DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+// 
+// But for a change, let"s do that for each and every word of the given String. 
+// Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+// 
+// *should work also on Leading and Trailing Spaces and caps.
+// 
+// "apple"            => "Apple"
+// "apple of banana"  => "Apple of Banana"
+// "one   space"      => "One   Space 
+// "   space WALK   " => "   Space Walk   " 
+// 
+// Note: you will be provided atleast one word and should take string as input and return string as output.
+//
+// Answer:
+function dropCap(n) {
+    return n.split(' ').map(e => e.length > 2 ? e[0].toUpperCase() + e.slice(1).toLowerCase() : e).join(' ');
+}
