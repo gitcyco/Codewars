4ff20ac9aab985ca2e1b5bd5ce90ad122a588b1d
Cycoder <mnerren@gmail.com>
2022-06-07 18:16:35 -0700

    new kata

---
+++ 7kyu/binaryScore.js
@@ -0,0 +1,17 @@
+// 7 kyu Binary scORe
+//
+// Objective
+// 
+// Given a number n we will define its scORe to be 0 | 1 | 2 | 3 | ... | n, where | is the bitwise OR operator.
+// 
+// Write a function that takes n and finds it's scORe.
+// n 	scORe n
+// 0 	0
+// 1 	1
+// 49 	63
+// 1000000 	1048575
+// 
+// Any feedback would be much appreciated
+//
+// Answer:
+const score = n => n<=0 ? 0 : parseInt(n.toString(2).replace(/0/g,'1'),2);
