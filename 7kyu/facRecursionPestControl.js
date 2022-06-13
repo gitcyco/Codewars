3a08cb72a6649fbf3e6c04221cb3169c03076b50
Cycoder <mnerren@gmail.com>
2022-06-13 16:13:07 -0700

    new kata

---
+++ 7kyu/facRecursionPestControl.js
@@ -0,0 +1,17 @@
+// 7 kyu Fac Recursion (Pest control - find the bugs and fix them)
+//
+// The code is a recursive attempt in calculating the result of a factorial function.
+// I.e. the result of 3! (Factorial of 3) is 3x2x1 = 6.
+// In case of 0! the result is 1 and in case of a negative factorial the result should be 0.
+// 
+// The goal is to find the bugs in the sourcecode and fix them (play a little pest control).
+//
+// Answer:
+function facRecursion(value){
+    if(value < 0)
+      return 0;
+    if(value === 1 || value === 0) {
+      return 1;
+    }
+    return value * facRecursion(value - 1);
+}
