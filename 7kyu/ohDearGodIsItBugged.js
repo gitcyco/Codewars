dd6398bfbc4b58d966741dabd462df422ecb8b4c
Cycoder <mnerren@gmail.com>
2022-06-15 10:43:21 -0700

    tidy

--- 7kyu/ohDearGodIsItBugged.js
+++ 7kyu/ohDearGodIsItBugged.js
@@ -8,7 +8,4 @@
 // P.S.- Random tests to be added soon.
 //
 // Answer:
-function isItBugged(code){
-    const regex = new RegExp(/^\d{2}-\d{2}-\d{4} \d{2}:\d{2}/);
-    return regex.test(code)
-}
+const isItBugged = code => new RegExp(/^(\d{2}-){2}\d{4} \d{2}:\d{2}/).test(code);
