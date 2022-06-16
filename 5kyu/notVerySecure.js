b0b5be274d27ac430dd065b82948f0bcbcdc2cd4
Cycoder <mnerren@gmail.com>
2022-06-16 12:15:35 -0700

    new kata

---
+++ 5kyu/notVerySecure.js
@@ -0,0 +1,12 @@
+// 5 kyu Not very secure
+//
+// In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+// 
+// The string has the following conditions to be alphanumeric:
+// 
+//     At least one character ("" is not valid)
+//     Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+//     No whitespaces / underscore
+//
+// Answer:
+const alphanumeric = string => string ? !(/[\s]|[^a-zA-Z0-9]/g.test(string)) : false;
