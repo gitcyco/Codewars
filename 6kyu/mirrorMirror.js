060bffcba73be1e1e59403c7fe5ea186b0f8ea64
Cycoder <mnerren@gmail.com>
2022-06-22 14:36:00 -0700

    new kata

---
+++ 6kyu/mirrorMirror.js
@@ -0,0 +1,20 @@
+// 6 kyu Mirror, Mirror
+//
+// Write a function evilTwin(obj) which returns a new object with all the same properties as obj, and with an additional property hasGoatee set to true.
+// 
+// For example:
+// 
+// var orig = {x: 5};
+// console.log(orig.x) // -> 5
+// console.log(orig.hasGoatee) // -> undefined
+// var twin = evilTwin(orig);
+// console.log(twin.x) // -> 5
+// console.log(twin.hasGoatee) // -> true
+// 
+// If the original object is modified, its twin should reflect the changes so:
+// 
+// orig.z = 12
+// console.log(twin.z) // -> 12
+//
+// Answer:
+const evilTwin = obj => ({ hasGoatee: true, __proto__: obj });
