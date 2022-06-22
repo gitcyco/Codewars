8c8bd1071faa2dee5bd867137fda53633cc644bf
Cycoder <mnerren@gmail.com>
2022-06-21 17:02:17 -0700

    new kata

---
+++ 7kyu/honeyToTheBee.js
@@ -0,0 +1,26 @@
+// 7 kyu Honey to the Bee
+// 
+// Find out why the amount of honey in the hive is not increasing.
+//
+// Answer:
+function Bee(capacity, hive) {
+    this.capacity = capacity;
+    this.hive = hive;
+}
+
+function Hive() {
+    this.pollen = 100;
+}
+
+//TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function (pollen) {
+    this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function () {
+    return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function () {
+    this.hive.addPollen(this.capacity);
+}
