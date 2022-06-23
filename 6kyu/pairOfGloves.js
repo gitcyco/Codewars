b506ce51072289e6c24a0cc31e2aeb08c5eee041
Cycoder <mnerren@gmail.com>
2022-06-22 17:41:52 -0700

    new kata

---
+++ 6kyu/pairOfGloves.js
@@ -0,0 +1,20 @@
+// 6 kyu Pair of gloves
+//
+// Pair of gloves
+// 
+// Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+// 
+// Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+// Examples:
+// 
+// input = ["red", "green", "red", "blue", "blue"]
+// result = 2 (1 red pair + 1 blue pair)
+// 
+// input = ["red", "red", "red", "red", "red", "red"]
+// result = 3 (3 red pairs)
+//
+// Answer:
+function numberOfPairs(gloves) {
+    const list = gloves.reduce((ac, e) => (ac[e] ? ac[e]++ : ac[e] = 1, ac), {})
+    return Object.values(list).reduce((ac, e) => ac += Math.floor(e / 2), 0)
+}
