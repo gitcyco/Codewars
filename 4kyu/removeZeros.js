83e676200b301ab03c697cee505ce860d61828f9
Cycoder <mnerren@gmail.com>
2022-06-26 11:50:43 -0700

    new kata

---
+++ 4kyu/removeZeros.js
@@ -0,0 +1,28 @@
+// 4 kyu Remove Zeros
+//
+// Write a function that takes an array of values and moves all elements that are zero to the end of the array, otherwise preserving the order of the array. 
+// The zero elements must also maintain the order in which they occurred.
+// 
+// For example, the following array
+// 
+// [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
+// 
+// is transformed into
+// 
+// [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+// 
+// Zero elements are defined by either 0 or "0". Some tests may include elements that are not number literals.
+// 
+// You are NOT allowed to use any temporary arrays or objects. You are also not allowed to use any Array.prototype or Object.prototype methods.
+//
+// Answer:
+function removeZeros(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = arr.length - 1; j > i; j--) {
+            if ((arr[j] !== 0 && arr[j] !== "0") && (arr[j - 1] === 0 || arr[j - 1] === "0")) {
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
