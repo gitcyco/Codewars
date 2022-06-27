ea35fc902ef0e79cc2a80315ec2bd35366d2a155
Cycoder <mnerren@gmail.com>
2022-06-26 23:11:26 -0700

    new kata

---
+++ 6kyu/buildTower.ts
@@ -0,0 +1,31 @@
+// 6 kyu Build Tower
+//
+// Build Tower
+// 
+// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+// 
+// For example, a tower with 3 floors looks like this:
+// 
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// 
+// And a tower with 6 floors looks like this:
+// 
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+//
+// Answer:
+export const towerBuilder = (n: number, arr: string[] = []): string[] => {
+    const max: number = n + n - 1;
+    for (let i = 0; i < max; i += 2) arr.push(' '.repeat((max - i) / 2) + "*".repeat(i + 1) + ' '.repeat((max - i) / 2));
+    return arr;
+}
