cf8b14a6e51318a471d50b9001c246083a795e08
Cycoder <mnerren@gmail.com>
2022-06-28 15:20:01 -0700

    new kata

---
+++ 6kyu/thisIsAnOtherSolution.js
@@ -0,0 +1,65 @@
+// 6 kyu "this" is an other solution
+//
+// In a previous kata ( http://www.codewars.com/kata/547f1a8d4a437abdf800055c ) we try to create an object with mutable properties.
+// 
+// The function NamedOne taken first & last names as parameters, returned an object with firstName, lastName and fullName ( = firstName + a space + lastName ) properties. 
+// These properties were mutable.
+// 
+// For example, with a "NamedOne" like this :
+// 
+// var namedOne = new NamedOne("Naomi","Wang")
+// namedOne.fullName  // -> "Naomi Wang"
+// 
+// ...we were able to change its properties:
+// 
+// namedOne.firstName = "John" 
+// namedOne.lastName = "Doe"
+// namedOne.fullName  // -> "John Doe" 
+// //..or..
+// namedOne.fullName = "Bill Smith"
+// namedOne.firstName // -> "Bill" 
+// namedOne.lastName // -> "Smith"
+// 
+// ##Your mission
+// 
+// On the contrary, the purpose of this kata is to create an OnceNamedOne object with immutable properties.
+// 
+// That means .firstName, .lastName and .fullName are defined into the Constructor and can't be no more changed.
+// 
+// The properties are still readable using the dot or bracket notation but there're not writable.
+// 
+// ##Examples:
+// 
+// var onceNamed = new OnceNamedOne("Naomi","Wang")
+// 
+// // ...then...
+// onceNamed.firstName // -> "Naomi"
+// onceNamed.lastName  // -> "Wang"
+// onceNamed.fullName // -> "Naomi Wang"
+// 
+// // ...if you try : ...
+// onceNamed.firstName = "Bill"
+// // ...or...
+// onceNamed['lastName'] = "Smith"
+// 
+// // ...then...
+// onceNamed['firstName'] // -> "Naomi"
+// onceNamed['lastName']  // -> "Wang"
+// onceNamed['fullName'] // -> "Naomi Wang"
+// 
+// // ..but you can still create other instances..
+// var otherOne = new OnceNamedOne("Don","Jones")
+// otherOne.fullName // -> "Don Jones"
+// 
+// Can you change our function to create such a OnceNamedOne object ?
+
+// Answer:
+class OnceNamedOne {
+    constructor(first, last) {
+        this._firstName = first;
+        this._lastName = last;
+    }
+    get fullName() { return this._firstName + ' ' + this._lastName; }
+    get firstName() { return this._firstName; }
+    get lastName() { return this._lastName; }
+}
