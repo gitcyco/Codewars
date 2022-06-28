340bca77b96a34bef7d42e6dfa51cbb3acdab9e0
Cycoder <mnerren@gmail.com>
2022-06-28 15:12:57 -0700

    fix

--- 6kyu/largestIntegerExponent.ts
+++ 6kyu/largestIntegerExponent.ts
@@ -4,6 +4,9 @@
 // if p<=1 the method should return null/None (throw an ArgumentOutOfRange exception in C#).
 //
 // Answer:
+//
+// This is too slow for Typescript, refactor:
+//
 function getExponent(n, p) {
     if (p < 2) return null;
     for (let i = Math.abs(n); i > 0; i--) {
