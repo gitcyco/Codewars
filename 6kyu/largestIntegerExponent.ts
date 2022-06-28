96013c2b77469caee394f1cd805bfe82b8fb0ec5
Cycoder <mnerren@gmail.com>
2022-06-28 15:12:16 -0700

    tmp kata

---
+++ 6kyu/largestIntegerExponent.ts
@@ -0,0 +1,13 @@
+// 6 kyu Largest integer exponent
+//
+// Write a method named getExponent(n,p) that returns the largest integer exponent x such that px evenly divides n. 
+// if p<=1 the method should return null/None (throw an ArgumentOutOfRange exception in C#).
+//
+// Answer:
+function getExponent(n, p) {
+    if (p < 2) return null;
+    for (let i = Math.abs(n); i > 0; i--) {
+        if (n % (p ** i) === 0) return i;
+    }
+    return 0;
+}
