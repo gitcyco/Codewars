b518f5bbfad1fe7235dc3864b2a3feed34deaaf4
Cycoder <mnerren@gmail.com>
2022-07-05 00:39:52 -0700

    upd

--- 6kyu/breakCamelCase.js
+++ 6kyu/breakCamelCase.js
@@ -9,3 +9,16 @@
 // 
 // Answer:
 const solution = str => str.replace(/[(A-Z)]/g, x => ' ' + x.toUpperCase()).trim();
+
+// long form:
+function solution(str) {
+    let out = '';
+    for (let i = 0; i < str.length; i++) {
+        if (i > 0 && isCap(str[i])) {
+            out += ' ' + str[i];
+        } else out += str[i];
+    }
+    return out;
+}
+
+const isCap = c => c === c.toUpperCase();
