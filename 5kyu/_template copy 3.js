5ed17ba02e9ef6ac25a8ac3b941ae7cf8b07d9da
Cycoder <mnerren@gmail.com>
2022-07-29 20:27:14 -0700

    sketching out kata

---
+++ 5kyu/_template copy 3.js
@@ -0,0 +1,178 @@
+// Brainscrambler
+//
+// Brainscrambler is an Esoteric programming language.
+//
+// A Brainscrambler program consists of a string containing
+//
+//     command characters
+//     decimal numbers, which are the input of the program.
+//
+// Brainscrabler memory is made up of three stacks storing integers : A, B, and C.
+// The "current stack" starts out at A, then is rotated to B, then to C, then back to A again, etc.
+// The "current value" is the number at the top of the current stack.
+//
+// In other words :
+//
+//     A is to the right of C and to the left of B
+//     B is to the right of A and to the left of C
+//     C is to the right of B and to the left of A
+//
+// Your task is to create an interpreter for Brainscrambler. You have to write the method read(input) of the object Interpreter.
+//
+// This method takes as parameter a string representing the source code of the Brainscrambler program.
+//
+// Its output shall be a string containing all the values that have been sent to the output during the execution of the program.
+//
+// You must execute the commands until you reach the end of the input string.
+// Commands
+//
+// The eleven possible commands in the Brainscrambler language:
+//
+//     + Increment the current number.
+//     - Decrement the current number.
+//     < Move the current number to the stack to the "left".
+//     > Move the current number to the stack to the "right".
+//     * Push a zero onto the current stack.
+//     ^ Pop the current number and discard it.
+//     # Rotate between stacks.
+//     , Input a number and push it to the current stack. This number will consist of the decimal digits
+//       immediatly to the right of the , character. After reading the number, the program resumes at the command to the right of the last digit.
+//     . Output the current number.
+//     [ Enter the loop.
+//     ] Jump to the corresponding [ bracket if the current number is > 0.
+//
+// Specifications
+//
+//     All stacks have an original value of 0
+//
+//     The input numbers are decimal and in the big-endian order.
+//
+//     When popping from the stack, if the stack is empty, the current number will be undefined. If the current number is undefined :
+//         If a . or < or > operation appears, do not add anything to the output or shift anything.
+//         If a + or - appears, set the current number to 0.
+//         If a ] appears, do not reenter the loop (as undefined > 0 === false).
+//
+//     Between read() calls :
+//         The output is reset;
+//         The stacks keep their values;
+//         The current stack stays the same.
+//
+//     The loops are do [] while loops, i.e. the loop body is executed BEFORE evaluating the current value
+//
+//     Loops can be nested
+//
+// Examples
+// Simple Program
+//
+// const interpreter = new Interpreter();
+// interpreter.read('*+.');
+// '1'
+//
+// Answer:
+const Interpreter = function () {
+  const stacks = [[0], [0], [0]];
+  let sIdx = 0;
+  let sPtr = stacks[sIdx];
+
+  return {
+    read: function (code) {
+      let out = [];
+      console.log(code, stacks, sPtr, sIdx);
+
+      while (codePtr < code.length) {
+        switch (code[codePtr]) {
+          case "#":
+            sIdx = ++sIdx % 3;
+            sPtr = stacks(sIdx);
+            break;
+          case ">":
+            memPtr++;
+            if (memPtr > tape.length - 1) tape.push(0);
+            break;
+          case "<":
+            memPtr--;
+            if (memPtr < 0) {
+              tape.unshift(0);
+              memPtr = 0;
+            }
+            break;
+          case "+":
+            tape[memPtr] = ++tape[memPtr] % 256;
+            break;
+          case "-":
+            tape[memPtr] = --tape[memPtr] < 0 ? 255 : tape[memPtr];
+            break;
+          case "c":
+            clip = tape[memPtr];
+            break;
+          case "p":
+            tape[memPtr] = clip;
+            break;
+          case "P":
+            out.push(String.fromCharCode(tape[memPtr]));
+            break;
+          case "N":
+            out.push(tape[memPtr]);
+            break;
+          case "T":
+            tape[memPtr] = (tape[memPtr] * 2) % 256;
+            break;
+          case "Q":
+            tape[memPtr] = tape[memPtr] ** 2 % 256;
+            break;
+          case "U":
+            tape[memPtr] = Math.floor(Math.sqrt(tape[memPtr])) % 256;
+            break;
+          case "L":
+            tape[memPtr] = (tape[memPtr] + 2) % 256;
+            break;
+          case "I":
+            tape[memPtr] = --tape[memPtr] < 0 ? 255 : tape[memPtr];
+            tape[memPtr] = --tape[memPtr] < 0 ? 255 : tape[memPtr];
+            break;
+          case "V":
+            tape[memPtr] = Math.floor(tape[memPtr] / 2) % 256;
+            break;
+          case "A":
+            tape[memPtr] = (tape[memPtr] + clip) % 256;
+            break;
+          case "B":
+            let count = clip;
+            for (let i = 0; i < count; i++) {
+              tape[memPtr] = --tape[memPtr] < 0 ? 255 : tape[memPtr];
+            }
+            break;
+          case "Y":
+            tape[memPtr] = (tape[memPtr] * clip) % 256;
+            break;
+          case "D":
+            tape[memPtr] = Math.floor(tape[memPtr] / clip) % 256;
+            break;
+
+          case "[":
+            if (tape[memPtr] != 0) {
+              stack.push(codePtr);
+            } else {
+              let counter = 0;
+              while (true) {
+                codePtr++;
+                if (!code[codePtr]) break;
+                if (code[codePtr] === "[") counter++;
+                else if (code[codePtr] === "]") {
+                  if (counter) counter--;
+                  else break;
+                }
+              }
+            }
+            break;
+          case "]":
+            codePtr = stack.pop() - 1;
+            break;
+        } // end switch
+        codePtr++;
+      }
+
+      return out.join("");
+    },
+  };
+};
