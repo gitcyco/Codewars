50f9d0189a0e7172183a8c99527609344479d7b5
Cycoder <mnerren@gmail.com>
2022-09-04 23:03:56 -0700

    new kata

---
+++ 4kyu/stripComments.js
@@ -0,0 +1,33 @@
+// 4 kyu Strip Comments
+//
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in.
+// Any whitespace at the end of the line should also be stripped out.
+//
+// Example:
+//
+// Given an input string of:
+//
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+//
+// The output expected would be:
+//
+// apples, pears
+// grapes
+// bananas
+//
+// The code would be called like so:
+//
+// var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// // result should == "apples, pears\ngrapes\nbananas"
+//
+// Answer:
+function solution(input, markers) {
+  let lines = input.split("\n");
+  markers = markers.map((e) => e.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
+  markers.forEach((e) => {
+    lines = lines.map((k) => k.replace(RegExp(`${e}([\\s\\S]*)$`), "").trim());
+  });
+  return lines.join("\n");
+}
