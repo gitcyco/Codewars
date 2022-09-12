07f5cf1bf3ef891568f75a41fcdde11abb36cf0c
Cycoder <mnerren@gmail.com>
2022-09-12 15:57:42 -0700

    new kata

---
+++ 5kyu/productOfConsecutiveFibNumbers.ts
@@ -0,0 +1,61 @@
+// 5 kyu Product of consecutive Fib numbers
+//
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+//
+//     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+//
+// such as
+//
+//     F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+//
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+//
+//     F(n) * F(n+1) = prod.
+//
+// Your function productFib takes an integer (prod) and returns an array:
+//
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+//
+// depending on the language if F(n) * F(n+1) = prod.
+//
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+//
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+//
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+// Some Examples of Return:
+//
+// (depend on the language)
+//
+// productFib(714) # should return (21, 34, true),
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+//
+// productFib(800) # should return (34, 55, false),
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true],
+// productFib(800) # should return [34, 55, false],
+// -----
+// productFib(714) # should return {21, 34, 1},
+// productFib(800) # should return {34, 55, 0},
+// -----
+// productFib(714) # should return {21, 34, true},
+// productFib(800) # should return {34, 55, false},
+//
+// Answer:
+export const productFib = (prod: number): [number, number, boolean] => {
+  let n1: number = 0;
+  let n2: number = 1;
+  let sum: number;
+  let num: number = 1;
+  for (let i: number = 0; i < num++; i++) {
+    if (n1 * n2 < prod) {
+      sum = n1 + n2;
+      n1 = n2;
+      n2 = sum;
+    } else if (n1 * n2 === prod) {
+      return [n1, n2, true];
+    } else return [n1, n2, false];
+  }
+  return [-1, -1, false];
+};
