4e3b7f6274d66b61dfdcf9a32892b25b8900bcc7
Cycoder <mnerren@gmail.com>
2022-10-06 20:26:28 -0700

    trying different methods

--- 5kyu/scramblies.js
+++ 5kyu/scramblies.js
@@ -24,3 +24,51 @@ function scramble(str1, str2) {
     } else return false;
   });
 }
+
+// Trying different methods below, all seem the same time complexity as the solution above.
+
+function scramble2(str1, str2) {
+  let obj2 = {};
+  for (let i = 0; i < str2.length; i++) {
+    if (obj2[str2[i]]) obj2[str2[i]]++;
+    else obj2[str2[i]] = 1;
+  }
+
+  let obj1 = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (obj2[str1[i]]) {
+      if (obj1[str1[i]]) obj1[str1[i]]++;
+      else obj1[str1[i]] = 1;
+    }
+  }
+
+  return Object.keys(obj2).every((e) => {
+    if (obj2[e] <= obj1[e]) {
+      return true;
+    } else return false;
+  });
+}
+
+function scramble3(str1, str2) {
+  const characterCounts = {};
+  for (let i = 0; i < str1.length; i++) {
+    const char1 = str1[i];
+    const char2 = str2[i];
+    if (char2) {
+      if (!characterCounts[char2]) {
+        characterCounts[char2] = 1;
+      } else {
+        characterCounts[char2] = characterCounts[char2] + 1;
+      }
+    }
+    if (char1) {
+      if (characterCounts[char1]) {
+        characterCounts[char1] = characterCounts[char1] - 1;
+      } else {
+        characterCounts[char1] = -1;
+      }
+    }
+  }
+
+  return Object.values(characterCounts).every((e) => e < 1);
+}
