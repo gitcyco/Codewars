d900ace84d46f3982d849f2173419587a4a7a045
Cycoder <mnerren@gmail.com>
2022-10-10 22:29:25 -0700

    small fix

--- 6kyu/findTheOddInt.js
+++ 6kyu/findTheOddInt.js
@@ -1,10 +1,10 @@
 // 6 kyu Find the odd int
 //
 // Given an array of integers, find the one that appears an odd number of times.
-// 
+//
 // There will always be only one integer that appears an odd number of times.
 // Examples
-// 
+//
 // [7] should return 7, because it occurs 1 time (which is odd).
 // [0] should return 0, because it occurs 1 time (which is odd).
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
@@ -13,6 +13,6 @@
 //
 // Answer:
 function findOdd(A) {
-    let obj = A.reduce((a,e) => (a[''+e] ? a[''+e]++ : a[e]=1 ,a),{})
-    for(let key in obj) if(obj[key] % 2 != 0) return +key;
+  let obj = A.reduce((a, e) => (a[e] ? a[e]++ : (a[e] = 1), a), {});
+  for (let key in obj) if (obj[key] % 2 != 0) return +key;
 }
