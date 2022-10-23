1b9d5857aed8acbf3f09b34bb177085ab34b990b
Cycoder <mnerren@gmail.com>
2022-10-23 13:38:24 -0700

    new kata

---
+++ 4kyu/nestingStructureComparison.js
@@ -0,0 +1,38 @@
+// 4 kyu Nesting Structure Comparison
+//
+// Complete the function/method (depending on the language) to return true/True when its argument is an array
+// that has the same nesting structures and same corresponding length of nested arrays as the first array.
+//
+// For example:
+//
+//  // should return true
+// [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );
+//
+//  // should return false
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );
+//
+// // should return true
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] );
+//
+// // should return false
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );
+//
+// For your convenience, there is already a function 'isArray(o)' declared and defined that returns true if its argument is an array, false otherwise.
+//
+// Answer:
+Array.prototype.sameStructureAs = function (other) {
+  if (this.length !== other.length) return false;
+  for (let i = 0; i < this.length; i++) {
+    if (isArray(this[i])) {
+      if (!isArray(other[i])) return false;
+      if (this[i].sameStructureAs(other[i])) return true;
+      else return false;
+    }
+  }
+  return true;
+};
+
+// since we dont have their internal defined isArray function, define it here for future reference:
+const isArray = (arr) => Array.isArray(arr);
