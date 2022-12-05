d170bc345e19da8cfaf5f581b1025bf246ad8190
Cycoder <mnerren@gmail.com>
2022-12-04 21:23:02 -0800

    new kata

---
+++ 5kyu/primeNumbers.js
@@ -0,0 +1,56 @@
+// 5 kyu Prime Numbers
+//
+// Preface
+//
+// A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+//
+// A more detailed description of prime numbers can be found at: http://en.wikipedia.org/wiki/Prime_number
+// The Problem
+//
+// You will need to create logic for the following two functions: isPrime(number) and getPrimes(start, finish)
+// isPrime(number)
+//
+// Should return boolean true if prime, otherwise false
+// getPrimes(start, finish)
+//
+// Should return a unique, sorted array of all primes in the range [start, finish] (i.e. both numbers inclusive).
+// Note that this range can go both ways - e.g. getPrimes(10, 1) should return all primes from 1 to 10 both inclusive.
+// Sample Input:
+//
+// isPrime(number):
+//
+// isPrime(0); // === false
+// isPrime(1); // === false
+// isPrime(2); // === true
+// isPrime(3); // === true
+// isPrime(4); // === false
+// isPrime(5); // === true
+//
+// getPrimes(start, finish):
+//
+// getPrimes(0, 0); // === []
+// getPrimes(0, 30); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+// getPrimes(30, 0); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+//
+// Answer:
+function isPrime(num) {
+  if (num < 1) return false;
+  let sqrtnum = Math.floor(Math.sqrt(num));
+  let prime = num !== 1;
+  for (let i = 2; i < sqrtnum + 1; i++) {
+    if (num % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
+
+function getPrimes(start, finish) {
+  if (finish < start) [start, finish] = [finish, start];
+  let arr = [];
+  for (let i = start; i <= finish; i++) {
+    if (isPrime(i)) arr.push(i);
+  }
+  return arr;
+}
