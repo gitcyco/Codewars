31234a33a72d08f4ee28f47b8e2ac7f278c5aee3
Cycoder <mnerren@gmail.com>
2022-12-04 22:09:49 -0800

    new kata

---
+++ 6kyu/theSearchForPrimesTwinPrimes.js
@@ -0,0 +1,38 @@
+// 6 kyu The search for Primes! Twin Primes!
+//
+// A twin prime is a prime number that is either 2 less or 2 more than another prime number -
+// for example, either member of the twin prime pair (41, 43).
+// In other words, a twin prime is a prime that has a prime gap of two.
+// Sometimes the term twin prime is used for a pair of twin primes; an alternative name for this is prime twin or prime pair.
+// (from wiki https://en.wikipedia.org/wiki/Twin_prime)
+//
+// Your mission, should you choose to accept it, is to write a function that counts the number of sets of twin primes from 1 to n.
+//
+// If n is wrapped by twin primes (n-1 == prime && n+1 == prime) then that should also count even though n+1 is outside the range.
+//
+// Ex n = 10
+//
+// Twin Primes are (3,5) (5,7) so your function should return 2!
+//
+// Answer:
+function twinPrime(n) {
+  if (n < 2) return 0;
+  let count = 0;
+  for (let i = 1; i < n; i++) {
+    if (isPrime(i) && isPrime(i + 2)) count++;
+  }
+  return count;
+}
+
+function isPrime(num) {
+  if (num < 1) return false;
+  let sqrtnum = Math.floor(Math.sqrt(num));
+  let prime = num !== 1;
+  for (let i = 2; i < sqrtnum + 1; i++) {
+    if (num % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
