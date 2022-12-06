4600dfc83d913027b160501d36dca136d6cdbb26
Cycoder <mnerren@gmail.com>
2022-12-05 18:09:01 -0800

    new kata

---
+++ 5kyu/regexPasswordValidation.js
@@ -0,0 +1,13 @@
+// 5 kyu Regex Password Validation
+//
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+//
+//     At least six characters long
+//     contains a lowercase letter
+//     contains an uppercase letter
+//     contains a digit
+//     only contains alphanumeric characters (note that '_' is not alphanumeric)
+//
+// Answer:
+// assign your RegExp to REGEXP
+const REGEXP = /^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\D*\d)[A-Za-z0-9]{6,}$/;
