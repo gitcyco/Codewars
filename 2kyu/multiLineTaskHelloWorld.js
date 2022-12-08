c96947895caa7153b697973f317824f37fd0286f
Cycoder <mnerren@gmail.com>
2022-12-08 14:33:33 -0800

    new kata

---
+++ 2kyu/multiLineTaskHelloWorld.js
@@ -0,0 +1,43 @@
+// 2 kyu Multi Line Task++: Hello World
+//
+// You need to write a function f that returns the string Hello, world!.
+//
+// Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+//
+// Hint: It's possible to complete this in 28 lines only.
+//
+// Answer:
+//
+// NOTE: autoformatter messed it up a bit, but the submitted answer was 2 chars per line
+f = [][
+  `\
+t\
+o\
+S\
+t\
+r\
+i\
+n\
+g\
+`
+][
+  `\
+b\
+i\
+n\
+d\
+`
+]`\
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!`;
