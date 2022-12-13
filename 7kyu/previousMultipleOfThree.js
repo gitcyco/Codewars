c0199f7669a992139a4b3d36362e310854b6cff9
Cycoder <mnerren@gmail.com>
2022-12-13 14:58:46 -0800

    new kata

---
+++ 7kyu/previousMultipleOfThree.js
@@ -0,0 +1,20 @@
+// 7 kyu Previous multiple of three
+//
+// Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
+//
+// Return n if the input is already a multiple of three, and if no such number exists, return null, a similar empty value, or -1.
+// Examples
+//
+// 1      => null
+// 25     => null
+// 36     => 36
+// 1244   => 12
+// 952406 => 9
+//
+// Answer:
+const prevMultOfThree = (n) => {
+  while (n % 3 !== 0 && n.toString().length > 1) {
+    n = +n.toString().slice(0, -1);
+  }
+  return n % 3 === 0 ? n : null;
+};
