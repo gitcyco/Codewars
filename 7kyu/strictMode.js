448ede1538c032eedab02d8308d11a77376bbc55
Cycoder <mnerren@gmail.com>
2022-12-15 14:24:27 -0800

    add shorter version

--- 7kyu/strictMode.js
+++ 7kyu/strictMode.js
@@ -3,7 +3,12 @@
 // Write a function that returns whether it is running in strict mode.
 //
 // Answer:
+//
+// ugly
 const isInStrictMode = () =>
   (function () {
     return !this;
   })();
+
+// or super short:
+const isInStrictMode = () => !this;
