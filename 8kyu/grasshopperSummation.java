6981ac11d46e88eb4b41e42c1bed046dd840ef25
Cycoder <mnerren@gmail.com>
2022-12-25 22:21:47 -0800

    new kata

---
+++ 8kyu/grasshopperSummation.java
@@ -0,0 +1,17 @@
+// 8 kyu Grasshopper - Summation
+// 
+// Summation
+// 
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+// 
+// For example (Input -> Output):
+// 
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+// 
+// Answer:
+public class GrassHopper {
+    public static int summation(int n) {
+      return n * (n + 1) / 2;
+    }
+}
