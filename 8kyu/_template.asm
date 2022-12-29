f14c252b61b0d01c69302d8b65723cacb8ada221
gitcyco <gitcyco@users.noreply.github.com>
2022-12-28 17:18:54 -0800

    new kata

---
+++ 8kyu/_template.asm
@@ -0,0 +1,4 @@
+; DESCRIPTION
+; 
+; Answer:
+SECTION .text
---
+++ 8kyu/simpleMultiplication.asm
@@ -0,0 +1,28 @@
+; 8 kyu Simple multiplication
+; 
+; This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+; 
+; Answer:
+SECTION .text
+global simple_multiplication
+
+; Multiplies and returns the argument by 8 if the argument is even, else 9 if the argument is odd.
+; arg0         = (int64_t) The argument to multiply.
+; return value = (int64_t) The result.
+simple_multiplication:
+;   xor rax, rax
+  mov eax, edi
+  test al, 1
+  jnz odd_fn
+  jz  even_fn
+  ret
+  
+even_fn:
+  mov edx, 0x8
+  mul edx
+  ret
+  
+odd_fn:
+  mov edx, 0x9
+  mul edx
+  ret
