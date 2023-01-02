fb1bc61c31ef6236befa77137cb3909189c61773
gitcyco <gitcyco@users.noreply.github.com>
2023-01-01 16:03:33 -0800

    new kata

---
+++ 5kyu/rgbToHex.py
@@ -0,0 +1,24 @@
+# 5 kyu RGB To Hex Conversion
+# 
+# The rgb function is incomplete. 
+# Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. 
+# Valid decimal values for RGB are 0 - 255. 
+# Any values that fall out of that range must be rounded to the closest valid value.
+# 
+# Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+# 
+# The following are examples of expected output values:
+# 
+# rgb(255, 255, 255) // returns FFFFFF
+# rgb(255, 255, 300) // returns FFFFFF
+# rgb(0,0,0) // returns 000000
+# rgb(148, 0, 211) // returns 9400D3
+# 
+# Answer:
+def rgb(r, g, b):
+    return "%0.2X%0.2X%0.2X" % (fix(r),fix(g),fix(b))
+
+def fix(n):
+    n = 0 if n < 0 else n
+    n = 255 if n > 255 else n
+    return n
