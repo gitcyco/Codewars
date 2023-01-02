618bb5155698efa7b5cf1871bd2a179f361eb641
gitcyco <gitcyco@users.noreply.github.com>
2023-01-02 14:21:14 -0800

    new kata

---
+++ 6kyu/zeroPlentifulArray.py
@@ -0,0 +1,34 @@
+# 6 kyu Zero-plentiful Array
+# 
+# An array is called zero-plentiful if it contains multiple zeros, and every sequence of zeros is at least 4 items long.
+# 
+# Your task is to return the number of zero sequences if the given array is zero-plentiful, oherwise 0.
+# Examples
+# 
+# [0, 0, 0, 0, 0, 1]  -->  1
+# # 1 group of 5 zeros (>= 4), thus the result is 1
+# 
+# [0, 0, 0, 0, 1, 0, 0, 0, 0]  -->  2
+# # 2 group of 4 zeros (>= 4), thus the result is 2
+# 
+# [0, 0, 0, 0, 1, 0]  -->  0
+# # 1 group of 4 zeros and 1 group of 1 zero (< 4)
+# # _every_ sequence of zeros must be at least 4 long, thus the result is 0
+# 
+# [0, 0, 0, 1, 0, 0]  -->  0
+# # 1 group of 3 zeros (< 4) and 1 group of 2 zeros (< 4)
+# 
+# [1, 2, 3, 4, 5]  -->  0
+# # no zeros
+# 
+# []  -->  0
+# # no zeros
+# 
+# Answer:
+def zero_plentiful(arr):
+    arr = ''.join(map(lambda e: 'X' if e != 0 else str(e), arr)).split('X')
+    arr = [n for n in arr if len(n) > 0]
+    for a in arr:
+        if len(a) < 4:
+            return 0
+    return len(arr)
