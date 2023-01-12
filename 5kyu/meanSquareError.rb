a689fb4177b00a9a0d5d035d4004b18bd07fc89a
gitcyco <gitcyco@users.noreply.github.com>
2023-01-11 23:39:32 -0800

    new kata

---
+++ 5kyu/meanSquareError.rb
@@ -0,0 +1,23 @@
+# 5 kyu Mean Square Error
+# 
+# Complete the function that
+# 
+#     accepts two integer arrays of equal length
+#     compares the value each member in one array to the corresponding member in the other
+#     squares the absolute value difference between those two values
+#     and returns the average of those squared absolute value difference between each member pair.
+# 
+# Examples
+# 
+# [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+# [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+# [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+# 
+# Answer:
+def solution (arr1, arr2)
+    output = 0.0
+    arr1.each_with_index do | val, index |
+      output += ((val - arr2[index]) * (val - arr2[index])).abs
+    end
+    return output / arr1.length
+  end
