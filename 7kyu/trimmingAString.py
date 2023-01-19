ff9c66169561c09658f73a4ab7d0bc8e127639d0
gitcyco <gitcyco@users.noreply.github.com>
2023-01-19 14:13:05 -0800

    new kata

---
+++ 7kyu/trimmingAString.py
@@ -0,0 +1,24 @@
+# 7 kyu Trimming a string
+# 
+# Return a function that will trim a string (the first argument given) if it is longer than the maximum
+# string length (the second argument given). The result should also end with "..."
+# 
+# These dots at the end also add to the string length.
+# 
+# So in the above example, trim("Creating kata is fun", 14) should return "Creating ka..."
+# 
+# If the string is smaller than or equal to 3 characters then the length of the dots is not added to the string length.
+# 
+# e.g. trim("He", 1) should return "H..."
+# 
+# If the string is smaller or equal than the maximum string length, then simply return the string with no trimming or dots required.
+# 
+# e.g. trim("Code Wars is pretty rad", 50) should return "Code Wars is pretty rad"
+# 
+# Answer:
+def trim(phrase, size):
+    if len(phrase) <= size:
+        return phrase
+    if len(phrase) < 4 or size < 4:
+        return phrase[0:size] + '...'
+    return phrase[0:size - 3] + '...'
