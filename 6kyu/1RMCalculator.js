bd75e050a0f30db7b7d1094212df2747b3e7d004
gitcyco <gitcyco@users.noreply.github.com>
2023-01-26 15:19:39 -0800

    new kata

--- 6kyu/1RMCalculator.js
+++ 6kyu/1RMCalculator.js
@@ -1,5 +1,19 @@
-// DESCRIPTION
+// 6 kyu 1RM Calculator
 //
+// You just got done with your set at the gym, and you are wondering how much weight you could
+// lift if you did a single repetition. Thankfully, a few scholars have devised formulas for this purpose (from Wikipedia) :
+// Epley
+// 1 RM=w(1+r30)\Large\text{1 RM} = w(1 + \frac{r}{30})1 RM=w(1+30r​)
+// McGlothin
+// 1 RM=100w101.3−2.67123r\Large\text{1 RM} = \frac{100w}{101.3 - 2.67123r}1 RM=101.3−2.67123r100w​
+// Lombardi
+// 1 RM=wr0.10\Large\text{1 RM} = wr^{0.10}1 RM=wr0.10
+//
+// Your function will receive a weight w and a number of repetitions r and must return your projected one repetition maximum.
+// Since you are not sure which formula to use and you are feeling confident, your function will return the largest
+// value from the three formulas shown above, rounded to the nearest integer.
+// However, if the number of repetitions passed in is 1 (i.e., it is already a one rep max), your function must return w.
+// Also, if the number of repetitions passed in is 0 (i.e., no repetitions were completed), your function must return 0.
 //
 // Answer:
 function calculate1RM(w, r) {
