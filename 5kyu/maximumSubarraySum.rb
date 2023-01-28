1ac85b7644165932f02ba8617406fba78ce45abf
gitcyco <gitcyco@users.noreply.github.com>
2023-01-27 18:13:19 -0800

    new kata

---
+++ 5kyu/maximumSubarraySum.rb
@@ -0,0 +1,22 @@
+# 5 kyu Maximum subarray sum
+# 
+# The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+# 
+# maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# // should be 6: [4, -1, 2, 1]
+# 
+# Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array.
+# If the list is made up of only negative numbers, return 0 instead.
+# 
+# Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+# 
+# Answer:
+def max_sequence(arr)
+  max = 0
+  sum = 0
+  for i in arr
+    max = max + i > 0 ? max + i : 0
+    sum = sum < max ? max : sum
+  end
+  return sum
+end
