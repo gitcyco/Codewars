0a06f20d7ce08a51640e15c6a5b2dcbd105836f1
gitcyco <gitcyco@users.noreply.github.com>
2023-02-07 22:04:05 -0800

    new kata

---
+++ 6kyu/manhattanDistance.ts
@@ -0,0 +1,24 @@
+// 6 kyu Manhattan Distance
+//
+// The distance formula can be used to find the distance between two points.
+// What if we were trying to walk from point A to point B, but there were buildings in the way?
+// We would need some other formula..but which?
+// Manhattan Distance
+//
+// Manhattan distance is the distance between two points in a grid
+// (like the grid-like street geography of the New York borough of Manhattan)
+// calculated by only taking a vertical and/or horizontal path.
+//
+// Complete the function that accepts two points and returns the Manhattan Distance between the two points.
+//
+// The points are arrays or tuples containing the x and y coordinate in the grid. You can think of x as the row in the grid, and y as the column.
+// Examples
+//
+// * Input [1, 1], [1, 1] => Output 0
+// * Input [5, 4], [3, 2] => Output 4
+// * Input [1, 1], [0, 3] => Output 3
+//
+// Answer:
+export function manhattanDistance(pA: [number, number], pB: [number, number]): number {
+  return Math.abs(pA[0] - pB[0]) + Math.abs(pA[1] - pB[1])
+}
