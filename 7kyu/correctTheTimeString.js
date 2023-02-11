171d1fd4ae75e838f71287e6dd3ff5657e504931
gitcyco <gitcyco@users.noreply.github.com>
2023-02-10 16:55:50 -0800

    new kata

---
+++ 7kyu/correctTheTimeString.js
@@ -0,0 +1,25 @@
+// 7 kyu Correct the time-string
+//
+// A new task for you!
+//
+//     You have to create a method, that corrects a given time string.
+//     There was a problem in addition, so many of the time strings are broken.
+//     Time is formatted using the 24-hour clock, so from 00:00:00 to 23:59:59.
+//
+// Examples
+//
+// "09:10:01" -> "09:10:01"
+// "11:70:10" -> "12:10:10"
+// "19:99:99" -> "20:40:39"
+// "24:01:01" -> "00:01:01"
+//
+// If the input-string is null or empty return exactly this value! (empty string for C++) If the time-string-format is invalid, return null. (empty string for C++)
+//
+// Answer:
+function timeCorrect(timestring) {
+  if (timestring === "") return timestring;
+  if (!/\d\d:\d\d:\d\d/.test(timestring)) return null;
+  let [hrs, mins, secs] = timestring.split(":").map(Number);
+  const seconds = hrs * 60 * 60 + mins * 60 + secs;
+  return new Date(seconds * 1000).toISOString().substring(11, 19);
+}
