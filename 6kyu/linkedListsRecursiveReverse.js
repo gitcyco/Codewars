67a193906ccf750d35115d050f53485fdaff0ae9
gitcyco <gitcyco@users.noreply.github.com>
2023-02-18 21:08:07 -0800

    new kata

---
+++ 6kyu/linkedListsRecursiveReverse.js
@@ -0,0 +1,26 @@
+// 6 kyu Linked Lists - Recursive Reverse
+//
+// Linked Lists - Recursive Reverse
+//
+// Write a Recursive Reverse() function that recursively reverses a linked list. You may want to use a nested function for the recursive calls.
+//
+// var list = 2 -> 1 -> 3 -> 6 -> 5 -> null
+// reverse(list) === 5 -> 6 -> 3 -> 1 -> 2 -> null
+//
+// Answer:
+function Node(data) {
+  this.data = data === undefined ? null : data;
+  this.next = null;
+}
+
+function reverse(head) {
+  if (!head || !head.next) return head;
+
+  const reverse = (cur, prev, next = null) => {
+    if (cur.next) next = cur.next;
+    cur.next = prev;
+    if (!next) return cur;
+    return reverse(next, cur);
+  };
+  return reverse(head, null);
+}
