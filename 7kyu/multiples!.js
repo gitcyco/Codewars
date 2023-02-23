278d8b81a794811aa563c9a082e394565c3b4b0d
gitcyco <gitcyco@users.noreply.github.com>
2023-02-23 15:45:24 -0800

    new kata

---
+++ 7kyu/multiples!.js
@@ -0,0 +1,10 @@
+// 7 kyu Multiples!
+//
+// Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3,
+// "Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5,
+// and "Miss" if it isn't divisible by any of them. Note: Your program should only return one value
+//
+// Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 25 --> Output: "Boom"
+//
+// Answer:
+const multiple = (x) => (x % 15 ? (x % 3 ? (x % 5 ? "Miss" : "Boom") : "Bang") : "BangBoom");
