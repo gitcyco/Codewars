12a6a0b0ed8bab831f1da34bcc3157afad53ce0c
gitcyco <gitcyco@users.noreply.github.com>
2023-03-17 16:23:53 -0700

    cleanup isPrime

--- 6kyu/transformToPrime.js
+++ 6kyu/transformToPrime.js
@@ -46,10 +46,7 @@ function isPrime(num) {
   let sqrtnum = Math.floor(Math.sqrt(num));
   let prime = num !== 1;
   for (let i = 2; i < sqrtnum + 1; i++) {
-    if (num % i === 0) {
-      prime = false;
-      break;
-    }
+    if (num % i === 0) return false;
   }
   return prime;
 }
