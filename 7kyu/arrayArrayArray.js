3ecd904fd86008a58d4b426ffe0a84a455e43116
gitcyco <gitcyco@users.noreply.github.com>
2023-03-20 16:59:59 -0700

    new kata

---
+++ 7kyu/arrayArrayArray.js
@@ -0,0 +1,20 @@
+// 7 kyu Array Array Array
+//
+// You are given an initial 2-value array (x). You will use this to calculate a score.
+//
+// If both values in (x) are numbers, the score is the sum of the two.
+// If only one is a number, the score is that number. If neither is a number, return 'Void!'.
+//
+// Once you have your score, you must return an array of arrays.
+// Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+//
+// For example:
+//
+// if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
+//
+// Answer:
+function explode(x) {
+  if (!x || x.every((e) => +e !== e || isNaN(e))) return "Void!";
+  let c = x.reduce((a, e) => (+e === e ? a + e : a), 0);
+  return new Array(c).fill(x);
+}
