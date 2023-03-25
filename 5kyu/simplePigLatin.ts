375bb852616e399618779d17cf9764d04b042ab0
gitcyco <gitcyco@users.noreply.github.com>
2023-03-25 16:48:49 -0700

    new kata

---
+++ 5kyu/simplePigLatin.ts
@@ -0,0 +1,12 @@
+// 5 kyu Simple Pig Latin
+//
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+// Examples
+// 
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+//
+// Answer:
+export const pigIt = (str : string) : string =>  {
+  return str.split(' ').map((e,i) => e.match(/[a-z]/i) ? e.slice(1) + e[0] + 'ay' : e).join(' ')
+}
