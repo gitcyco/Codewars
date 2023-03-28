bbb3632de8146bebe6d054be29f02e1fe4455ab3
gitcyco <gitcyco@users.noreply.github.com>
2023-03-27 18:34:01 -0700

    new kata

---
+++ 6kyu/sumsOfConsecutiveIntegers.py
@@ -0,0 +1,25 @@
+# 7 kyu Sums of consecutive integers
+# 
+# The sum of x consecutive integers is y. What is the consecutive integer at position n?
+# Given x, y, and n, solve for the integer. Assume the starting position is 0.
+# 
+# For example, if the sum of 4 consecutive integers is 14, what is the consecutive integer at position 3?
+# 
+# We find that the consecutive integers are [2, 3, 4, 5], so the integer at position 3 is 5.
+# 
+# position(4, 14, 3) == 5
+# 
+# Assume there will always be a sum of x consecutive integers that totals to y and n will never be indexed out of bounds.
+# 
+# Answer:
+# 
+# Given the number of consecutive integers and the total of the integers,
+# return the consecutive integer at the requested position.
+#
+# Input: 
+#    x number of consecutive integers
+#    y sum of consecutive integers
+#    n position of requested integer
+#
+# return integer at requested position
+position = lambda x,y,n: n + (y - (x * (x - 1)) / 2) / x
