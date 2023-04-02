3c861cb7222375905c6996b14bb703acee090fb6
gitcyco <gitcyco@users.noreply.github.com>
2023-04-01 17:32:35 -0700

    new kata

---
+++ 6kyu/exerciseInSumming.js
@@ -0,0 +1,38 @@
+// 6 kyu Exercise in Summing
+//
+// Your task is to finish two functions, minimumSum and maximumSum, that take 2 parameters:
+//
+//     values: an array of integers with an arbitrary length; may be positive and negative
+//     n: how many integers should be summed; always 0 or bigger
+//
+// Example:
+//
+// var values = [5, 4, 3, 2, 1];
+// minimumSum(values, 2); // should return 1+2 = 3
+// maximumSum(values, 3); // should return 3+4+5 = 12
+//
+// All values given to the functions will be integers. Also take care of the following special cases:
+//
+//     if values is empty, both functions should return 0
+//     if n is 0, both functions should also return 0
+//     if n is larger than values's length, use the length instead.
+//
+// Answer:
+function minimumSum(values, n) {
+  values.sort((a, b) => a - b);
+  return getSum(values, n);
+}
+
+function maximumSum(values, n) {
+  values.sort((a, b) => b - a);
+  return getSum(values, n);
+}
+
+const getSum = (arr, n) => {
+  let sum = 0;
+  if (arr.length < n) n = arr.length;
+  for (let i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
