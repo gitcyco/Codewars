d882d7e37ac51cec2818ba7290ef66c7bee7fb7a
gitcyco <gitcyco@users.noreply.github.com>
2023-05-23 15:46:22 -0700

    new kata

---
+++ 7kyu/reverseANumberInAnyBase.js
@@ -0,0 +1,36 @@
+// 7 kyu Reverse a number in any base
+//
+// Write a function which when given a non-negative integer n and an arbitrary base b, returns the number reversed in that base.
+// Examples
+//
+//     n=12 and b=2 return 3, because 12 in binary is "1100", which reverses to "0011", equivalent to 3 in decimal.
+//     n=10 and b=5 return 2, because 10 in base-5 is "20", which reverses to "02", equivalent to 2 in decimal.
+//     n=45 and b=30 return 451, because 45 in base-30 is "1F", which reverses to "F1", equivalent to 451 in decimal.
+//     n=3 and b=4 return 3, because 3 in base-4 is "3", which reverses to "3", equivalent to 3 in decimal.
+//     n=5 and b=1 return 5, because 5 in unary is "|||||", which reverses to "|||||", equivalent to 5 in decimal.
+//
+// Note
+//
+//     The function should return an integer.
+//     The base b will be greater or equal to 1.
+//     Edge case: Base-1 is also used in this kata to represent the unary numeral system. Think of it as a system of tally marks. For example:
+//         3 (decimal) -> ||| (unary)
+//         5 (decimal) -> ||||| (unary)
+//
+// Answer:
+function reverseNumber(n, b) {
+  // Input n and b are provided as BigInt values
+  if (b === 1n) return n;
+  let arr = [];
+  while (n > 0n) {
+    let dig = n % b;
+    arr.push(dig);
+    n = n / b;
+  }
+  arr = arr.reverse();
+  let tot = 0n;
+  for (let i = 0n; i < arr.length; i++) {
+    tot += arr[i] * b ** i;
+  }
+  return tot;
+}
