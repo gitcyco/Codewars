db3a6bff099fd3215da39c0d67e16488ad9b0287
gitcyco <gitcyco@users.noreply.github.com>
2023-07-10 17:41:22 -0700

    new kata

---
+++ 6kyu/takeATenMinutesWalk.py
@@ -0,0 +1,30 @@
+# 6 kyu Take a Ten Minutes Walk
+# 
+# You live in the city of Cartesia where all roads are laid out in a perfect grid.
+# You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+# The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button
+# it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']).
+# You always walk only a single block for each letter (direction) and you know it takes you one minute
+# to traverse one city block, so create a function that will return true if the walk the app gives you will
+# take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+#
+#     Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only).
+#     It will never give you an empty array (that's not a walk, that's standing still!).
+# 
+# Answer:
+def is_valid_walk(walk):
+    if len(walk) != 10:
+        return False
+    dirs = {
+        'n': 1,
+        's': -1,
+        'w': -1,
+        'e': 1
+    }
+    x,y = [0,0]
+    xDirs = list(filter(lambda d: d == 'n' or d == 's', walk))
+    yDirs = list(filter(lambda d: d == 'e' or d == 'w', walk))
+    for d in walk:
+        x = x + dirs[d] if d in xDirs else x 
+        y = y + dirs[d] if d in yDirs else y 
+    return x == 0 and y == 0
