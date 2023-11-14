ff26c38ea699c974b73d283eb2c796253febd2c8
gitcyco <gitcyco@users.noreply.github.com>
2023-11-14 12:38:56 -0800

    move some stuff outside the class

--- 3kyu/primeStreamingPG-13.js
+++ 3kyu/primeStreamingPG-13.js
@@ -3,61 +3,50 @@
 // Create an endless stream that yields prime numbers. The stream must be able to produce a million primes in a few seconds.
 //
 // Answer:
+const primes = [];
+function sieve(limit, primes) {
+  let mark = new Array(limit + 1).fill(true);
+  for (let p = 2; p * p < limit; p++) {
+    if (mark[p] === true) {
+      for (let i = p * p; i < limit; i += p) {
+        mark[i] = false;
+      }
+    }
+  }
+  for (let p = 2; p < limit; p++) {
+    if (mark[p] === true) {
+      primes.push(p);
+    }
+  }
+}
 class Primes {
   static *stream() {
     const primes = [];
-    function sieve(limit, primes) {
-      let mark = new Array(limit + 1).fill(true);
-      for (let p = 2; p * p < limit; p++) {
-        if (mark[p] === true) {
-          for (let i = p * p; i < limit; i += p) {
-            mark[i] = false;
-          }
-        }
-      }
-      for (let p = 2; p < limit; p++) {
-        if (mark[p] === true) {
-          primes.push(p);
-        }
-      }
-    }
-
     const n = Number.MAX_SAFE_INTEGER;
-
-    let limit = 100000;
-
-    sieve(limit, primes);
+    let limit = 1000000;
+    if (primes.length === 0) sieve(limit, primes);
     for (let p of primes) yield p;
 
     let low = limit;
     let high = 2 * limit;
 
     while (low < n) {
-      if (high >= n) {
-        high = n;
-      }
+      if (high >= n) high = n;
 
       let mark = new Array(limit + 1).fill(true);
 
       for (let i = 0; i < primes.length; i++) {
         let loLim = Math.floor(low / primes[i]) * primes[i];
-        if (loLim < low) {
-          loLim += primes[i];
-        }
+        if (loLim < low) loLim += primes[i];
         for (let j = loLim; j < high; j += primes[i]) {
           mark[j - low] = false;
         }
       }
-
       for (let i = low; i < high; i++) {
-        if (mark[i - low] == true) {
-          yield i;
-        }
+        if (mark[i - low] == true) yield i;
       }
       low = low + limit;
       high = high + limit;
     }
-
-    return;
   }
 }
