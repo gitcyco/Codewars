5fba9cc40ee84325ac71b0855d506e30583596d7
gitcyco <gitcyco@users.noreply.github.com>
2024-02-24 16:41:50 -0800

    new kata

---
+++ 8kyu/flick_switch.rs
@@ -0,0 +1,31 @@
+// 8 kyu Flick Switch
+//
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+// Examples
+//
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+//
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+//
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+//
+// Notes
+//
+//     "flick" will always be given in lowercase.
+//     A list may contain multiple flicks.
+//     Switch the boolean value on the same element as the flick itself.
+//
+// Answer:
+fn flick_switch(mut list: &[&str]) -> Vec<bool> {
+    let mut switch: bool = true;
+    return list
+        .iter()
+        .map(|&n| {
+            if n == "flick" {
+                switch = !switch;
+            }
+            return switch;
+        })
+        .collect();
+}
