e5b26e88eb82d0910445d6a0d4f73a16b39487f3
gitcyco <gitcyco@users.noreply.github.com>
2024-04-12 19:27:31 -0700

    new kata

---
+++ 6kyu/calculator(Addition and Spaces)part1.js
@@ -0,0 +1,26 @@
+// 6 kyu Calculator (Addition and Spaces) [Part 1]
+//
+// Given the string "1+2+3", write a function calculate to output the result, ie. 6.
+//
+// If there is no operator between 2 numbers, there will be a space instead ie. the input is "1+2 3".
+// In this case, process the input as "1+23" and output 24.
+//
+// The input will always be a string with 3 integers 0-1000, zero or more + operators, and zero or more spaces.
+//
+// Assume the input will always be valid.
+//
+// Note: eval and Function have been disabled; if not there would be no point to this exercise.
+//
+// Examples:
+//
+// Test.assertEquals(calculate("1+2+3"), 6);
+// Test.assertEquals(calculate("1+2 3"), 24);
+// Test.assertEquals(calculate("1 2 3"), 123);
+//
+// Answer:
+const calculate = (s) =>
+  s
+    .replace(/\s/g, "")
+    .split("+")
+    .map(Number)
+    .reduce((a, e) => a + e, 0);
