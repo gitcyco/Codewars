5fdd5ee6ed6b22d3c6059f55d138a238cb4a13d0
gitcyco <gitcyco@users.noreply.github.com>
2024-08-23 14:29:04 -0700

    new kata

---
+++ 5kyu/scrapingGetTheYearACodeWarriorJoined.py
@@ -0,0 +1,27 @@
+# 5 kyu Scraping: Get the Year a CodeWarrior Joined
+# 
+# Write a function that accepts a username from someone at Codewars and returns 
+# a string containing the month and year (separated by a space) when they joined Codewars.
+# Example:
+# 
+#       Input            Output
+# ----------------     ----------
+# "donaldsebleung" --> "Jan 2016"
+#       "jhoffner" --> "Oct 2012"
+# 
+# Answer:
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_member_since(username):
+    url = f"https://www.codewars.com/users/{username}"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        divs = soup.find_all(filter_tags, class_="stat")
+        for div in divs:
+            if div.b.string == "Member Since:":
+                return div.b.next_sibling
+
+def filter_tags(tag):
+    return tag.b and tag.b.string == "Member Since:"
