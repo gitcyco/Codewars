3d3e2f218b1306901b2d6b78c6b74d3b8bbd934a
gitcyco <gitcyco@users.noreply.github.com>
2024-09-07 10:06:30 -0700

    new kata

---
+++ 6kyu/partitionOn.js
@@ -0,0 +1,32 @@
+// 5 kyu Partition On
+//
+// Write a function which partitions a list of items based on a given predicate.
+//
+// After the partition function is run, the list should be of the form [ F, F, F, T, T, T ]
+// where the Fs (resp. Ts) are items for which the predicate function returned false (resp. true).
+//
+// NOTE: the partitioning should be stable; in other words: the ordering of the Fs (resp. Ts) should be preserved relative to each other.
+//
+// For convenience and utility, the partition function should return the boundary index.
+// In other words: the index of the first T value in items.
+//
+// For example:
+//
+// var items = [1, 2, 3, 4, 5, 6];
+// function isEven(n) {return n % 2 == 0}
+// var i = partitionOn(isEven, items);
+// items should now be [1, 3, 5, 2, 4, 6]
+// i     should now be 3
+//
+// Answer:
+function partitionOn(pred, items) {
+  let f = items.filter((e) => !pred(e));
+  let t = items.filter((e) => pred(e));
+  for (let i = 0; i < f.length; i++) {
+    items[i] = f[i];
+  }
+  for (let i = 0; i < t.length; i++) {
+    items[i + f.length] = t[i];
+  }
+  return f.length;
+}
