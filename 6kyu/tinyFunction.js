2dc29ed0aef3c395895d3f265ca5b21074565cec
gitcyco <gitcyco@users.noreply.github.com>
2025-04-07 15:27:30 -0700

    new kata

---
+++ 6kyu/tinyFunction.js
@@ -0,0 +1,15 @@
+// 6 kyu "Tiny" function
+//
+// Our universe is so huge, possibly even infinite and it makes us feel so tiny, isn't that so?
+// Task
+//
+// Write a tiny function f that outputs 'tiny' without using the letters t, i, n, y anywhere in your code
+// Code Limit
+//
+// Your code is limited to less than 34 characters
+//
+// Buffer and unicode escapes \ are banned to avoid cheesy solutions :)
+//
+// Answer:
+// prettier-ignore
+f=_=>(a=1/0+"")[6]+a[3]+a[4]+a[7]
