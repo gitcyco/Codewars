b17fc93d37332a2633af097d553b5eb037b6e61a
gitcyco <gitcyco@users.noreply.github.com>
2025-04-27 15:59:12 -0700

    new kata

---
+++ 6kyu/santasMissingGiftList.js
@@ -0,0 +1,43 @@
+// 6 kyu Santa's Missing Gift List
+//
+// Santa has misplaced his list of gift to all the children, he has however a condensed version lying around.
+//
+// In this condensed verison, instead of a list of gifts for each child, each one has an integer.
+//
+// He also have a list of gifts corresponding to each integer. His list is as follows:
+//
+// GIFTS = {
+//     1 => 'Toy Soldier',
+//     2 => 'Wooden Train',
+//     4 => 'Hoop',
+//     8 => 'Chess Board',
+//    16 => 'Horse',
+//    32 => 'Teddy',
+//    64 => 'Lego',
+//   128 => 'Football',
+//   256 => 'Doll',
+//   512 => "Rubik's Cube"
+// }
+//
+// This list is made available to you, as GIFTS.
+//
+// The integer for each child is such that the child should get the highest toy lower
+// than or equal to that integer, and then, if there's more left, also get the highest
+// toy lower than the rest and so on. Know that Santa never gives the same gift twice.
+//
+// For example, by Timmy's name is 160. This means that Timmy should get both
+// a football and a teddy, because 128 + 32 = 160.
+//
+// You should help Santa by decoding his own list and recreate the missing list for him.
+// Santa's elf wants the list sorted alphabetically by the toys, so you should help them as well and list the toys in a sorted order.
+//
+// Answer:
+function gifts(n) {
+  let num = 0b1000000000; // 200h === 512
+  const bag = [];
+  while (num > 0) {
+    if (num & n) bag.push(GIFTS[num]);
+    num >>= 1;
+  }
+  return bag.sort((a, b) => a.localeCompare(b));
+}
