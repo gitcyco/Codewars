ec58d3a6a71368253aa8983e7df3e1e959210de5
gitcyco <gitcyco@users.noreply.github.com>
2026-04-19 17:28:03 -0700

    new kata

---
+++ 6kyu/findCharacter.py
@@ -0,0 +1,79 @@
+# 6 kyu Find character
+# 
+#     When no more interesting kata can be resolved, I just choose to create the new kata, to solve their own, to enjoy the process --myjinxin2015 said
+# 
+# Description:
+# 
+# Give you a character matrix. 
+# Find out the character which has the smallest amount.
+# 
+# Arguments:
+# 
+#     matrix: A string that contains some letters. Each row is separated by "\n".
+# 
+# Results & Note:
+# 
+#     Returns the characters which has smallest amount, using string format.
+#     If more than one characters are found, sort them according to the order A-Za-z0-9.
+#     You can assume that there are at least two characters in the matrix.
+#     Please ignore the "\n" ;-)
+# 
+# Some Examples
+# 
+# matrix=
+# 00000000
+# 0000O000
+# 00000000
+# 00000000
+# 00000000
+# 
+# result -> "O"
+# 
+# matrix=
+# mmmmmmmmmmmmm
+# mmmmmmmmmmmmm
+# mmmmmmmmmmmmm
+# mmmmmmmmmnmmm
+# 
+# result -> "n"
+# 
+# matrix=
+# AAAAAAAAAAA
+# AABBBBBBBBB
+# BCCCCCCCCDD
+# DDDDEEEEFFF
+# 
+# result -> "F"
+# 
+# matrix=
+# AAAAA
+# ABBBB
+# BCCCC
+# DDDDE
+# EEEEF
+# FFFFF
+# 
+# result -> "CD"
+# 
+# Answer:
+from collections import Counter
+import re
+
+def find_characters(matrix):
+    counts = dict(Counter(matrix))
+    counts.pop('\n', None)
+    counts = dict(sorted(counts.items(), key=lambda item: item[1]))
+    min = None
+    output = ""
+    for key, value in counts.items():
+        if min == None or value < min:
+            min = value
+            output = key
+        elif value == min:
+            output = output + key
+        else:
+            output = "".join(sorted(output))
+            numpart = re.findall(r'\d+', output)
+            output = re.sub(r'\d+', "", output)
+            return output + "".join(numpart)
+    
